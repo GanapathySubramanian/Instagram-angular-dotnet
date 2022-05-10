77d8413eb3b2e418b49cf5f210234020194100bc
Ganapathy Subramanian S <ganapathy5subramanian@gmail.com>
2022-05-10 11:18:07 +0530

    like and unlike

---
+++ backend/Instagram/Instagram/Models/Comment.cs
@@ -0,0 +1,17 @@
+﻿namespace Instagram.Models
+{
+    public class Comment
+    {
+        public int commentId { get; set; }
+
+        public int userId { get; set; }
+
+        public string username { get; set; }
+
+        public string profile { get; set; }
+
+        public string text { get; set; }
+
+        public int postId { get; set; }
+    }
+}
